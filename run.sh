3cdc25740f67a88e9f4263fb766e9547a7ab0958
Zeein <42746957+zeineth@users.noreply.github.com>
2024-03-20 13:08:23 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-d9a97b5c-07b2-4326-b578-2e1318ba33b9/)
 
 
 echo "$curl_output" >> Update.tsx
